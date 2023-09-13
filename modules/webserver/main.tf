adcf24033b37194ca0982eca3e05832f2d0fbf90
ajayteju <ajay.chaudhari007@gmail.com>
2023-09-13 16:38:12 +0530

    modularized terraform project

---
+++ modules/webserver/main.tf
@@ -0,0 +1,68 @@
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = var.vpc_id
+      tags = {
+         Name: "${var.env_prefix}-sg"
+    }
+}   
+
+resource "aws_security_group_rule" "public_in_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip]
+  security_group_id = aws_security_group.myapp-sg.id
+}
+resource "aws_security_group_rule" "public_in_http" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.myapp-sg.id
+}
+resource "aws_security_group_rule" "public_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.myapp-sg.id
+}  
+
+data "aws_ami" "latest-amazon-linux-image" {
+      most_recent = true
+      owners = ["amazon"] 
+      filter {
+         name = "name"
+         values = [var.image_name]
+      } 
+      filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+      }
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = "${file(var.public_key_location)}"
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id] 
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name 
+
+
+    user_data = file("entry-script.sh")
+
+      tags = {
+         Name: "${var.env_prefix}-server "
+    }
+}
